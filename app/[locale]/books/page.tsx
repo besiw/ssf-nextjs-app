c5b2ae99e8c12823b52f40ffa5c4f01164a77958
admin <1025897081@.com>
2024-05-31 14:48:41 +0800

    update file name

--- app/[locale]/books/page.tsx
+++ app/[locale]/books/page.tsx
@@ -2,20 +2,12 @@ import Header from "@/components/Header";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import heroImage from "../../assets/img/intl_icon.png";
-// import BooksNotes from "../../../components/book/BooksNotes";
-import BooksTitle from "../../../components/book/BooksTitle";
 
 import MainTemplate from "@/components/MainTemplate";
 import BookList from "@/app/[locale]/books/widgets/BooksList";
 import FilterSide from "@/app/[locale]/books/widgets/FilterSide";
 import BooksTitle from "@/app/[locale]/books/widgets/SidebarBut";
 export default function Home() {
-  return (
-    <div>
-      <BooksTitle />
-      {/* <BooksNotes /> */}
-    </div>
-  );
   return (
     <MainTemplate
       TopSection={<BooksTitle />}
--- app/[locale]/books/widgets/package/Books.tsx
+++ app/[locale]/books/widgets/package/Books.tsx
@@ -1,36 +1,31 @@
-'use client';
+"use client";
 
-<<<<<<< HEAD
 import Image, { StaticImageData } from "next/image";
-import Heart from "../../../../../components/book/Heart";
-=======
-import Image, { StaticImageData } from 'next/image';
 
-import { Link } from '@/middleware';
-import Heart from './Heart';
->>>>>>> fd6af46a04a4f94781d5fe1420fc4dc6b0881a38
+import { Link } from "@/middleware";
+import Heart from "@/components/book/Heart";
 type BooksProps = {
-	path?: StaticImageData | undefined;
-	text?: string | undefined;
-	author?: string;
-	age?: string;
+  path?: StaticImageData | undefined;
+  text?: string | undefined;
+  author?: string;
+  age?: string;
 };
 const Books: React.FC<BooksProps> = ({ path: src, text, age, author }) => {
-	const path = `/book`;
-	return (
-		<div className="flex flex-col mb-6 md:flex-row w-1/2 xl:w-1/3 ">
-			<Link href={path}>
-				<Image src={src} alt={text} height={'200'} width={'150'} />
-			</Link>
-			<div className="pl-5 0.5sm:pt-3 flex-1">
-				<Heart />
-				<Link href={path}>
-					<div className="max-h-15 text-11.5">{text}</div>
-					<div className=" text-10">{author}</div>
-					<div className="text-10">{age}</div>
-				</Link>
-			</div>
-		</div>
-	);
+  const path = `/book`;
+  return (
+    <div className="flex flex-col mb-6 md:flex-row w-1/2 xl:w-1/3  mx-4">
+      <Link href={path}>
+        <Image src={src} alt={text} height={"200"} width={"150"} />
+      </Link>
+      <div className="pl-5 0.5sm:pt-3 flex-1">
+        <Heart />
+        <Link href={path}>
+          <div className="max-h-15 text-11.5">{text}</div>
+          <div className=" text-10">{author}</div>
+          <div className="text-10">{age}</div>
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default Books;
