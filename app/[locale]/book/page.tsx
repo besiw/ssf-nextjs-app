c35e8d636e21240b9267616c499cda740f8a4146
admin <1025897081@.com>
2024-05-31 17:21:24 +0800

    update file name

--- app/[locale]/book/page.tsx
+++ app/[locale]/book/page.tsx
@@ -1,22 +1,28 @@
-import MainTemplate from '@/components/MainTemplate';
-import BookContent from './widgets/BookPagerWrapper';
-import BooksTitle from './widgets/BooksTitle';
-
+import MainTemplate from "@/components/MainTemplate";
+import BookContent from "./BookContent";
+import BooksTitle from "./BooksTitle";
+import NoteColBlk from "./widgets/NoteColBlk";
 export default function Home() {
-	return (
-		<>
-			<MainTemplate
-				TopSection={
-					<BooksTitle
-						bookTitle="Stykker og referater i fornyet utgave fra Skjulte Skatter av Johan
+  return (
+    <>
+      {/* <MainTemplate
+        TopSection={
+          <BooksTitle
+            bookTitle="Stykker og referater i fornyet utgave fra Skjulte Skatter av Johan
 			O.Smith"
-						bookAuthor="Kare J. Smith"
-					/>
-				}
-				LeftColumn={<div />}
-				RightColumn={<div />}
-				MainSection={<BookContent />}
-			/>
-		</>
-	);
+            bookAuthor="Kare J. Smith"
+          />
+        }
+        LeftColumn={<div />}
+        RightColumn={<div />}
+        MainSection={<BookContent />}
+      /> */}
+      <NoteColBlk
+        title="‘Lorem ipsum dolor sit amet...’"
+        content="Lorem ipsum dolor sit amet consectetur. Tortor vulputate cursus lobortis consequat congue."
+        color="bg-pink-200"
+        userName="Burcu A."
+      />
+    </>
+  );
 }
